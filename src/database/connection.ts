94a9126974f89b14bb8b360dbc14f9966eb5e03f
mahaabi01 <mahaabi01@gmail.com>
2025-02-09 17:49:47 +0545

    code without reconnection on each trigger after setting 0.0.0.0/0 on db

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,17 +2,20 @@ import mongoose from "mongoose";
 
 const MONGODB_CS = process.env.MONGODB_CS;
 
-
-//this practice of connecting database is bad way as there is problem like for each request the database is connected each time which is not a expected behaviour so we follow alternate database connection method.
+//this is the solution for connecting database without reinitializing of database each time
 if (!MONGODB_CS) {
   throw new Error("You must provide Mongodb connection string.");
 }
 const dbConnect = async () => {
+  if(mongoose.connection.readyState === 1){
+    console.log("Database already connected !");
+    return;
+  }
   try {
     await mongoose.connect(MONGODB_CS);
     console.log("Database connected successfully. 🙂");
   } catch (error) {
-    console.log("Error connecting...😒😒", error);
+    console.log("Error connecting...😒", error);
   }
 };
 
