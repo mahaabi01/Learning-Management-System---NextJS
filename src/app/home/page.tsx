b8b8a2cc135979d0f31963c3a1d23157a63bd5da
mahaabi01 <mahaabi01@gmail.com>
2025-02-10 17:32:33 +0545

    Session Wrapper and Image component of nextJS

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,27 @@
 "use client"
 
-import {signIn} from "next-auth/react"
+import {signIn, signOut, useSession} from "next-auth/react"
+import Image from "next/image"
 
-function Hello(){
+function Home(){
+  const {data:session} = useSession()
+  if(session){
+    return (
+      <>
+      <Image src={session.user?.image} alt="Your Image" width={80} height={80}
+      />
+      <h1>Welcome, {session.user?.name}</h1>
+      <h3>Email: {session.user?.email}</h3>
+      <button onClick={()=>signOut()}>Sign Out</button>
+      </>
+    )
+  }
   return (
     <div>
+      <h1>Not Logged In</h1>
       <button onClick={()=>signIn("google")}>Sign in With Google</button>
     </div>
   )
 }
 
-export default Hello
+export default Home
