80d394eced0a9f88a18b98d7bc78a4e82f0be25d
mahaabi01 <mahaabi01@gmail.com>
2025-03-16 12:21:14 +0545

    code with some error

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categorySlice from './category/categorySlice'
+import courseSlice from './courses/courseSlice'
+import lessonSlice from './lessons/lessonSlice'
 
 
 //in react js we directly invoke cofigureStore function but for react js we have to wrap it inside another function.
 export const makeStore = () => {
   return configureStore({
     reducer: {
-      categories: categorySlice
+      categories: categorySlice,
+      courses: courseSlice,
+      lessons: lessonSlice
     },
   })
 }
